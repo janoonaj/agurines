4452de075578bea6a8bc4a5adacbe8dc1ca5b3e3
janoonaj <jano_alex_es@hotmail.com>
2016-09-01 15:30:41 +0200

    Level2

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,9 +5,6 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour {
 
 	public void changeScene(int level) {
-        if(level == 0) {
-            SceneManager.LoadScene("SelectScreen");
-        } else 
-            SceneManager.LoadScene("level" + level);
+        SceneManager.LoadScene(level);
     }
 }
