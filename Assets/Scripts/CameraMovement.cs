9001d3d0a95137014c8654011e7ff653e6ceeba2
janoonaj <jano_alex_es@hotmail.com>
2016-09-01 13:40:03 +0200

    Add margin to camera movement
    Solve problems with Y axis on camera movement.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
-    public GameObject player;
+    private GameObject player;
     private const float smoothTimeX = 0.05f;
     private const float smoothTimeY = 0.05f;
     private Vector2 velocity;
     private float[] MARGIN_X = { 0.65f, 0.35f };
-    private float[] MARGIN_Y = { 0.55f, 0.45f };
+    private float[] MARGIN_Y = { 0.8f, 0.2f };
+
+    public void Start() {
+        player = GameObject.Find("five");
+    }
 
     //TODO: soft the camera movement a little bit with smoothdamp
     void FixedUpdate () {
@@ -16,16 +20,34 @@ public class CameraMovement : MonoBehaviour {
         Vector3 playerMove = player.GetComponent<Five>().currentPos - player.GetComponent<Five>().lastPos;
         Vector3 target = transform.position + playerMove;
 
-        if (viewPosition.x > MARGIN_X[0] || viewPosition.x < MARGIN_X[1]) {
+        if ((viewPosition.x > MARGIN_X[0] && movingRight(playerMove)) || 
+            (viewPosition.x < MARGIN_X[1] && movingLeft(playerMove))) {
             //float posX = Mathf.SmoothDamp(transform.position.x, target.x, ref velocity.x, smoothTimeX);
             float posX = target.x;
             transform.position = new Vector3(posX, transform.position.y, transform.position.z);
         }
 
-        if (viewPosition.y > MARGIN_Y[0] || viewPosition.y < MARGIN_Y[1]) {
+        if ((viewPosition.y > MARGIN_Y[0] && movingUp(playerMove))
+            || (viewPosition.y < MARGIN_Y[1]) && movingDown(playerMove)) {
             //float posY = Mathf.SmoothDamp(transform.position.y, target.y, ref velocity.y, smoothTimeY);
             float posY = target.y;
             transform.position = new Vector3(transform.position.x, posY, transform.position.z);
         }
+    }
+
+    private bool movingRight(Vector3 playerMove) {
+        return playerMove.x > 0;
+    }
+
+    private bool movingLeft(Vector3 playerMove) {
+        return playerMove.x < 0;
+    }
+
+    private bool movingUp(Vector3 playerMove) {
+        return playerMove.y > 0;
+    }
+
+    private bool movingDown(Vector3 playerMove) {
+        return playerMove.y < 0;
     }
 }
