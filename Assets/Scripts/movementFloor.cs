21b4d474abbb8d7c4e77a489c4626f4d69bb9873
janoonaj <jano_alex_es@hotmail.com>
2016-09-22 21:53:25 +0200

    Force to movement floor
    Floors on movement will apply a force to the ball. The idea is to make
    easier to control the bal when its on a moving platform.

--- Assets/Scripts/movementFloor.cs
+++ Assets/Scripts/movementFloor.cs
@@ -10,14 +10,15 @@ public class MovementFloor : MonoBehaviour {
     private Vector3 startPos;
     private bool waiting = false;
     private float waitingTimeCount = 0;
+    private AreaEffector2D effector;
 
 
     void Start() {
         startPos = transform.position;
         direction = finalPos - startPos;
+        effector = GetComponentInChildren<AreaEffector2D>();
     }
 	
-	// Update is called once per frame
 	void Update () {
         updateWaitingTime();
         if (waiting) return;
@@ -28,7 +29,8 @@ public class MovementFloor : MonoBehaviour {
                             transform.position.z);
         
         if(platformArrived(nextPos)) {
-            switchDirection();            
+            switchDirection();
+            stopForce();          
             waiting = true;
         }
 
@@ -65,7 +67,23 @@ public class MovementFloor : MonoBehaviour {
         if(waitingTimeCount >= waitingTime) {
             waitingTimeCount = 0;
             waiting = false;
+            startForce();
         }
+    }
+
+    /*
+     * Control the ball on a platform, when its moving, is very difficult for the player.
+     * When the platform moves add a fake force, with the direction of the movement,
+     * to make it easier.
+     */
+    private void startForce() {
+        const float FAKE_FORCE = 1f;
+        effector.forceAngle = Vector3.Angle(Vector3.right, direction);
+        effector.forceMagnitude = FAKE_FORCE;
+    }
+
+    private void stopForce() {
+        effector.forceMagnitude = 0f;
     }
 }
 
