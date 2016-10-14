b61fdb246a2592828af97b0d62a37a317befb7e1
janoonaj <jano_alex_es@hotmail.com>
2016-10-14 12:33:26 +0200

    Max FPS set on 30fps

--- Assets/Scripts/ConfigureGame.cs
+++ Assets/Scripts/ConfigureGame.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class ConfigureGame : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    void Awake() {
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Application.targetFrameRate = 30;
+        QualitySettings.vSyncCount = 0;
     }
 }
